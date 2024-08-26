a1dd89e216716ddb72dcaaaddf2fd4043a3a31c0
bhawnav09 <bhawnav09@gmail.com>
2024-08-26 21:30:04 +0530

    access Specifiers

---
+++ access_specifier2/Class_1.java
@@ -0,0 +1,21 @@
+package access_specifier2;
+
+import access_specifier.Classpackage2;
+
+public class Class_1 extends Classpackage2 {
+
+	public static void main(String[] args) {
+		Class_1 c1 = new Class_1();
+		c1.method1();
+		 c1.method4();
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 // it only access public and protected method in differnet package
+	}
+
+}
